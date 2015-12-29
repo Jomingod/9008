d3084618d93bc2f45d33fd5cf584553d082c65a9
李腾达 <tarena2025@tengdadeimac.local>
2015-12-29 15:28:31 +0800

    hhhhh

---
+++ hhhh/hhhh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hhhh
+//
+//  Created by tarena2025 on 15/12/29.
+//  Copyright © 2015年 tenda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ hhhh/hhhh/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  hhhh
+//
+//  Created by tarena2025 on 15/12/29.
+//  Copyright © 2015年 tenda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
